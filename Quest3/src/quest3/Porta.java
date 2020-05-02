8a44a60e4882f3b87578524ae613d783bb2d1d78
Alef-Araujo <64667160+Alef-Araujo@users.noreply.github.com>
2020-05-01 21:46:00 -0400

    Add files via upload

---
+++ Quest3/src/quest3/Porta.java
@@ -0,0 +1,32 @@
+package quest3;
+
+public class Porta {
+    Boolean aberta;
+    String cor = "amarela";
+    int dimensaoX;
+    int dimensaoY;
+    int dimensaoZ;
+
+    void abre(){
+        aberta = true;
+    }
+
+    void fecha(){
+	aberta = false;
+    }
+
+    void pinta(String s){
+	this.cor = s;
+    }
+
+
+    boolean estaAberta() {
+	if (this.aberta == true) {
+            System.out.println("Está aberta");
+            return true;
+        } else {
+	System.out.println("Está fechada");
+	return false;
+        }         
+    }
+}
